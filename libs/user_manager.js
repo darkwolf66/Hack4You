17c2a21bc1c31681af6316538bf1656f223deaee
darkwolf_66 <will.moraes.96@gmail.com>
2017-02-21 09:30:38 -0300

    Added to libs user manager

---
+++ libs/user_manager.js
@@ -0,0 +1,29 @@
+var config    = require('../config.js');
+var microtime = require('microtime');
+var lang = require('../lang.js');
+var lang = new lang();
+var rlang = lang.returnMessage;
+
+var UserManager = function() {
+	this.checkUserLogin = function(token, res, lang){
+		var user = global.loggedUsers.get(token);
+		if(user != undefined){
+			if(res != undefined && global.loggedUsers.get(token).actions > config.expiration.actions){
+				global.loggedUsers.delete(token);
+				return false;
+			}
+			if(microtime.now() < user.token_expiration){
+				global.loggedUsers.get(token).actions++;
+				global.loggedUsers.get(token).minute_actions++;
+				return true;
+			}else{
+				global.loggedUsers.delete(token);
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+}
+
+module.exports = UserManager;
