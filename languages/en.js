328b40636b5ff30388aefa97faf6bff9b7343253
darkwolf_66 <will.moraes.96@gmail.com>
2017-02-21 09:26:49 -0300

    New errors and added Registration email

--- languages/en.js
+++ languages/en.js
@@ -48,6 +48,35 @@ module.exports = {
 		{
 			code: 12,
 			message:"Account already exists!"
+		},
+		{
+			code: 13,
+			message:"No account found with this data!"
+		},
+		{
+			code: 14,
+			message:"Invalid password!"
+		},
+		{
+			code: 15,
+			message:"Critical error please contact the admin!"
+		},
+		{
+			code: 16,
+			message:"Please login to use this action!"
+		},
+		{
+			code: 17,
+			message:"Please login again you reach max actions per token!"
+		}
+	],
+	mails:[
+		{
+			code: 1,
+			body:{
+				subject:"Your account creation has been successfull!",
+				content: "Welcome to Hack4You. Now you can play and view an world of hacker and be the hacker, enjoy!"
+			}
 		}
 	]
 }
