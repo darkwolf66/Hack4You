2bf0d82f1544d963b7ad7a357d30b74f7d489a07
darkwolf_66 <will.moraes.96@gmail.com>
2017-02-22 20:36:52 -0300

    added route MachineQuery

---
+++ routes/machine_query.js
@@ -0,0 +1,71 @@
+var lang = require('../lang.js');
+var config  =  require('../config.js');
+var microtime = require('microtime');
+//Presets
+var lang = new lang();
+var rlang = lang.returnMessage;
+
+var moment = require('moment');
+
+module.exports = function(app, my){
+    app.post('/api/query/machine/', function(req, res){
+        //Checa linguagem
+        if(!req.body.lang){
+            return res.send({"status": "error", "code":1 ,"message": 'Blank Language'});
+        }else{
+            var lang = req.body.lang;
+        }
+        //User auth agent
+        var rsp = global.userManager.checkUserLogin(req.body.token, res, lang);
+        if(rsp == 'exit'){
+            return false;
+        }
+        if(req.body.token && rsp){
+            //Pega a maquina do player atual pelo ip e pega a maquina destino, compara o power
+            if(!req.body.myip || !req.body.atackip){
+                return res.send({"status": "error", "code":21 ,"message": rlang(lang, 21)});
+            }
+            my.query('SELECT * FROM 4h_machines WHERE ip=?', req.body.myip, function (error, own_machine, fields) {
+                if (error) {
+                    console.log(error);
+                    return res.send({"status": "error", "code":15 ,"message": rlang(lang, 15)});
+                }else if(own_machine.lenght <= 0){
+                    return res.send({"status": "error", "code":19 ,"message": rlang(lang, 19)});
+                }else{
+                    //Checa se a maquina atacante é sua
+                    own_machine = own_machine[0];
+                    if(own_machine.id_user == global.loggedUsers.get(req.body.token).id_user){
+                        //Fazer join para pegar o balance
+                        //Checa os bots
+                        var atacked = global.machineBots.get(req.body.atackip);
+                        if(atacked){
+                            if(own_machine.scan > atacked.firewall){
+                                return res.send({"status": "success", "machine": atacked});
+                            }else{
+                                return res.send({"status": "error", "code":20 ,"message": rlang(lang, 20)});
+                            }
+                        }else{
+                            my.query('SELECT * FROM 4h_machines WHERE ip=?', req.body.atackip, function (error, atacked, fields) {
+                                //Checa se os status da sua maquina vs atacado
+                                if(error){//Avisa se a machine com este ip não existe
+                                    return res.send({"status": "error", "code":15 ,"message": rlang(lang, 15)});
+                                }else if(atacked.lenght == undefined || atacked.lenght <= 0){
+                                    return res.send({"status": "error", "code":19 ,"message": rlang(lang, 19)});
+                                }else if(own_machine.scan > atacked.firewall){
+                                    atacked = atacked[0];
+                                    return res.send({"status": "success", "machine": atacked});
+                                }else{
+                                    return res.send({"status": "error", "code":20 ,"message": rlang(lang, 20)});
+                                }
+                            });
+                        }
+                    }else{
+                        return res.send({"status": "error", "code":18 ,"message": rlang(lang, 18)});
+                    }
+                }
+            });
+        }else{
+            return res.send({"status": "error", "code":16 ,"message": rlang(lang, 16)});
+        }
+    });
+}
