d6ea67ce3f2c1d911b4c4e111e86a15f6b76b31e
darkwolf_66 <will.moraes.96@gmail.com>
2017-03-01 13:29:49 -0300

    Actions check fix

--- tasks/actions_check.js
+++ tasks/actions_check.js
@@ -1,39 +1,21 @@
-var lang = require('../lang.js');
 var config  =  require('../config.js');
-var fieldValidator = require('../libs/field_validator');
 var microtime = require('microtime');
-//Presets
-var lang = new lang();
-var rlang = lang.returnMessage;
 
-module.exports = function(app, my){
-    app.post('/api/query/machine/', function(req, res){
-        if(!req.body.lang){
-            return res.send({"status": "error", "code":1 ,"message": 'Blank Language'});
-        }else{
-            var lang = req.body.lang;
+var ActionsCheck = function() {
+    var checkTask;
+    var userlistCheck = function(){
+        console.log('User cleaner started.. By ActionsCheck');
+        for (var user of global.loggedUsers.values()) {
+            if(user.minute_actions > config.expiration.minute_actions){
+                global.loggedUsers.delete(user.token);
+                console.log('User with token: '+user.token+' has been disconnected.. by ActionsCheck with reason: Max Minute Actions');
+            }else{
+                global.loggedUsers.get(user.token).minute_actions = 0;
+            }
         }
-        var rsp = global.userManager.checkUserLogin(req.body.token, res, lang);
-        if(rsp == 'exit'){
-            return false;
-        }
-        if(req.body.token && rsp){
-        	if(){
-        		
-        	}
-            my.query('SELECT * FROM 4h_machines WHERE id_user=?', global.loggedUsers.get(token).id_user, function (error, own_machine, fields) {
-                if (error) {
-                    console.log(error);
-                    return res.send({"status": "error", "code":15 ,"message": rlang(lang, 15)});
-                }else{
-                    money = result[0].money;
-                    return res.send({"status": "success", "balance": money});
-                }
-            });
-        }else{
-            return res.send({"status": "error", "code":16 ,"message": rlang(lang, 16)});
-        }
-    });
-
+    }
+    this.startTask = function(){
+        checkTask = setInterval(userlistCheck, config.tasks.actions_check);
+    }
 }
-
+module.exports = new ActionsCheck();
